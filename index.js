f8917e05325d20a1f8b02c4ad89f2d8520bb47d1
EmilianoVentura7 <223314@ids.upchiapas.edu.mx>
2025-05-19 23:23:45 -0600

    changes file index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.status(200).send('Login OK, DevOps');
+  res.status(200).send('Login OK, Dev');
 });
 
 
